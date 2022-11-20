c5ef36cb65dc0fc1217cfcb0744eef2136faa0d1
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-20 20:21:10 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,25 @@
 import express from "express";
-import { } from "../controllers/user.js";
-
+import {update,deleteUser,getUser,subscribe,unsubscribe,like,dislike} from "../controllers/user.js";
+import {verifyToken} from "../verifyToken.js";
 const router= express.Router();
 
+//update a user
+router.put("/:id",verifyToken,update);
+//delete a user
+router.delete("/:id",verifyToken,deleteUser);
+
+//get a user
+router.get("/find/:id",getUser);
+//subscribe a user
+router.put("/sub/:id",verifyToken,subscribe);
+
+//unsubscribe a user
+router.put("/unsub/:id",verifyToken,unsubscribe);
+
+
+//like a video
+router.put("/like/:videoId",verifyToken,like);
+
+//dislike a video
+router.put("/dislike/:videoId",verifyToken,dislike);
 export default router;
