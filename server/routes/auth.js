7ea31b8db585b542b22d626fe5e6229411905384
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-20 20:19:42 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { signup } from "../controllers/auth.js";
+import { signup,signin } from "../controllers/auth.js";
 
 const router= express.Router();
 
 //CREATE A USER 
 router.post("/signup",signup)
 //sIGN IN 
-router.post("/signin",)
+router.post("/signin",signin)
 
 //Google Auth
 router.post("/google",)
