227dc2392fa950b74bd8d6f9593be7c1528f15fb
Ghostly842 <soham842@gmail.com>
2022-11-23 20:35:45 +0530

    Frontend Commit
    First Commit

---
+++ Frontend/src/components/Menu.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import image from "../img/logo.png"
+import HomeIcon from "@mui/icons-material/Home"; 
+
+const Container = styled.div`
+flex:1;
+background-color:#202020;
+height:200vh;
+color:white;
+font-size:14px
+`;
+
+
+
+const Wrapper = styled.div`
+padding: 18px 26px
+
+`
+
+const Logo = styled.div`
+  display:flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+   
+`;
+
+const Img= styled.img`
+height: 25px;
+`
+
+const  Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap:20px;
+    cursor:pointer;
+`
+
+
+const Menu =()=>{
+    return(
+        <Container>
+            <Wrapper>
+                <Logo>
+                    <Img src={image}/>
+                    Stream.io
+                </Logo>
+                <Item>
+                  <HomeIcon/>  
+                  Home   
+                </Item>
+            </Wrapper>
+        </Container>
+    )
+}
+
+
+export default Menu 
