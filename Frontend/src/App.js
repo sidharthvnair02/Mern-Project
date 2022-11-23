7057b2a6286eabf7b8d72a280380dcb6898923c5
Ghostly842 <soham842@gmail.com>
2022-11-24 01:16:27 +0530

    Card.jsx started

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,10 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+
 
 const Container = styled.div`
   display: flex;
@@ -24,12 +28,22 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Main>
-          <Navbar/>
-          <Wrapper>Video Cards</Wrapper>
-        </Main>
-        {/* {main} */}
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home/>} />
+                  <Route path="video">
+                  <Route path=":id" element={<Video/>}/>
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
       </Container>
     </ThemeProvider>
   );
---
+++ Frontend/src/pages/Video.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+
+
+const Video = () => {
+  return (
+  <Container>
+    
+  </Container>
+     );
+};
+
+export default Video;
