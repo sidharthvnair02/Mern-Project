88a3d47f910e844adb8b83d07969a91760ae1c92
Ghostly842 <soham842@gmail.com>
2022-11-26 17:41:49 +0530

    Day 7

---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 56px);
+  color: ${({ theme }) => theme.text};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.bgLighter};
+  border: 1px solid ${({ theme }) => theme.soft};
+  padding: 20px 50px;
+  gap: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+`;
+
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+  width: 100%;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+
+const SignIn = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Sign in</Title>
+        <SubTitle>to continue to Stream.io</SubTitle>
+        <Input placeholder="Username" />
+        <Input type="password" placeholder="Password" />
+        <Button>Sign in</Button>
+        <Title>OR</Title>
+        <Input placeholder="Username" />
+        <Input placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+        <Button>Sign up</Button>
+      </Wrapper>
+    </Container>
+  );
+};
+
+
+export default SignIn;
