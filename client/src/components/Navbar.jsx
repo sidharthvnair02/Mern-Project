5fd7b41805ea40f3a66ee8e7b69039f419bcfdb8
mahir mehta <mahirmehta77@gmail.com>
2022-11-27 01:03:26 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const Container = styled.div`
   position: sticky;
@@ -33,6 +34,14 @@ const Button = styled.button`
   gap: 5px;
 `;
 
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: 500;
+  color: ${({theme}) => theme.text};
+`;
+
 const Search = styled.div`
   width: 40%;
   position: absolute;
@@ -54,8 +63,16 @@ const Input = styled.input`
   color: ${({ theme }) => theme.text};
 `;
 
+const Avatar = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: #999;
+`;
 
 const Navbar = () => {
+
+  const {currentUser} = useSelector(state=>state.user.currentUser)
   return (
     <Container>
       <Wrapper>
@@ -63,12 +80,18 @@ const Navbar = () => {
           <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Link to="signin" style={{textdecoration:"none"}}>
+        {currentUser ? (
+          <User>
+            <VideocallOutlinedIcon/>
+            <Avatar/>
+            {currentUser.name}
+          </User>
+        ) : <Link to="signin" style={{textdecoration:"none"}}>
           <Button>
             <AccountCircleOutlinedIcon />
             SIGN IN
           </Button>
-        </Link>
+        </Link>}
       </Wrapper>
     </Container>
   );
