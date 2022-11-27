30102bd5f0efc5f0272b4678073bbebd1724746b
mahir mehta <mahirmehta77@gmail.com>
2022-11-27 12:28:15 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import {createError} from "../error.js";
+import { createError } from "../error.js";
 import jwt from "jsonwebtoken";
 
 export const signup=async (req,res,next)=>{
@@ -13,13 +13,12 @@ export const signup=async (req,res,next)=>{
         await newUser.save();
         res.status(200).send("User has been created")
     }catch(err){
-        next(err)
+        next(err);
     }
-}
+};
 
 export const signin=async (req,res,next)=>{
     try{
-
         const user=await User.findOne({name:req.body.name})
         if(!user) return next(createError(404,"user not found"))
         
