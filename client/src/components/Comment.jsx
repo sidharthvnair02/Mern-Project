fd0051dcf9df3848f4bc4b55c037ed44bb3fbea1
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-27 23:25:00 +0530

    Update Comment.jsx

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -35,22 +36,25 @@ const Text = styled.span`
   font-size: 14px;
 `;
 
+const Comment = ({ comment }) => {
+  const [channel, setChannel] = useState({});
 
+  useEffect(() => {
+    const fetchComment = async () => {
+      const res = await axios.get(`/users/find/${comment.userId}`);
+      setChannel(res.data)
+    };
+    fetchComment();
+  }, [comment.userId]);
 
-const Comment = () => {
   return (
     <Container>
-      <Avatar/>
+      <Avatar src={channel.img} />
       <Details>
         <Name>
-          Soham Shah <Date>2 day ago</Date>
+          {channel.name} <Date>1 day ago</Date>
         </Name>
-        <Text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
-          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
-          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
-          mollitia totam reiciendis?
-        </Text>
+        <Text>{comment.desc}</Text>
       </Details>
     </Container>
   );
