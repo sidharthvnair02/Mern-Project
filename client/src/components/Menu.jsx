c171bd947a6e5b203fe2c991d00a9e363db0c119
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-27 23:26:18 +0530

    Update Menu.jsx

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../img/logo.png"
-import HomeIcon from "@mui/icons-material/Home"; 
+import logo from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
 import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
 import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
 import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
 import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
 import { Link } from "react-router-dom";
-
-
-
+import { useSelector } from "react-redux";
 const Container = styled.div`
   flex: 1;
   background-color: ${({ theme }) => theme.bgLighter};
@@ -55,20 +60,37 @@ const Hr = styled.hr`
 `;
 
 const Login = styled.div``;
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
 
-
-
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
 
 const Menu = ({ darkMode, setDarkMode }) => {
-
   const { currentUser } = useSelector((state) => state.user);
+
   return (
     <Container>
       <Wrapper>
         <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
           <Logo>
             <Img src={logo} />
-            Stream.io
+            Stream.Io
           </Logo>
         </Link>
         <Item>
@@ -114,7 +136,7 @@ const Menu = ({ darkMode, setDarkMode }) => {
             <Hr />
           </>
         }
-        <Title>Best of Stream.io</Title>
+        <Title>Best of Stream.Io</Title>
         <Item>
           <LibraryMusicOutlinedIcon />
           Music
@@ -161,5 +183,4 @@ const Menu = ({ darkMode, setDarkMode }) => {
   );
 };
 
-
-export default Menu 
+export default Menu;
