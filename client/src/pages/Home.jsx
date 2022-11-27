fa526d98b1d4f672bc2cc53da8c25fd8a3f5da96
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-27 23:17:00 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,28 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
-import axios from "axios"
-
-
+import axios from "axios";
 const Container = styled.div`
-  display:flex;
-  justify-content:space-between;
-  flex-wrap :wrap
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
 
 const Home = ({type}) => {
+  const [videos, setVideos] = useState([]);
 
-  const [videos,setVideos] = useState([])
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get(`/videos/${type}`);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, [type]);
 
-  useEffect(()=>{
-    const fetchVideos = async ()=>{
-        const res = await axios.get(`/videos/${type}`)
-        setVideos(res.data)
-    } 
-    fetchVideos()
-  },[type] )
   return (
     <Container>
       {videos.map((video) => (
-        <Card  key={video._id} video={video}/>
+        <Card key={video._id} video={video}/>
       ))}
     </Container>
   );
