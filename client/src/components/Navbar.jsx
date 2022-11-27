2d87c109ea772946ce68df57884807fda2892690
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-27 23:26:46 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Upload from "./Upload";
 
 const Container = styled.div`
   position: sticky;
@@ -19,6 +21,28 @@ const Wrapper = styled.div`
   height: 100%;
   padding: 0px 20px;
   position: relative;
+`;
+
+const Search = styled.div`
+  width: 40%;
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+  outline: none;
+  color: ${({ theme }) => theme.text};
 `;
 
 const Button = styled.button`
@@ -39,27 +63,6 @@ const User = styled.div`
   align-items: center;
   gap: 10px;
   font-weight: 500;
-  color: ${({theme}) => theme.text};
-`;
-
-const Search = styled.div`
-  width: 40%;
-  position: absolute;
-  left: 0px;
-  right: 0px;
-  margin: auto;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-`;
-
-const Input = styled.input`
-  border: none;
-  background-color: transparent;
-  outline: none;
   color: ${({ theme }) => theme.text};
 `;
 
@@ -71,29 +74,40 @@ const Avatar = styled.img`
 `;
 
 const Navbar = () => {
-
-  const {currentUser} = useSelector(state=>state.user)
+  const navigate = useNavigate()
+  const [open, setOpen] = useState(false);
+  const [q, setQ] = useState("");
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <Container>
-      <Wrapper>
-        <Search>
-          <Input placeholder="Search" />
-          <SearchOutlinedIcon />
-        </Search>
-        {currentUser ? (
-          <User>
-            <VideocallOutlinedIcon/>
-            <Avatar scr={currentUser.img}/>
-            {currentUser.name}
-          </User>
-        ) : <Link to="signin" style={{textdecoration:"none"}}>
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
-        </Link>}
-      </Wrapper>
-    </Container>
+    <>
+      <Container>
+        <Wrapper>
+          <Search>
+            <Input
+              placeholder="Search"
+              onChange={(e) => setQ(e.target.value)}
+            />
+            <SearchOutlinedIcon onClick={()=>navigate(`/search?q=${q}`)}/>
+          </Search>
+          {currentUser ? (
+            <User>
+              <VideoCallOutlinedIcon onClick={() => setOpen(true)} />
+              <Avatar src={currentUser.img} />
+              {currentUser.name}
+            </User>
+          ) : (
+            <Link to="signin" style={{ textDecoration: "none" }}>
+              <Button>
+                <AccountCircleOutlinedIcon />
+                SIGN IN
+              </Button>
+            </Link>
+          )}
+        </Wrapper>
+      </Container>
+      {open && <Upload setOpen={setOpen} />}
+    </>
   );
 };
+
 export default Navbar;
