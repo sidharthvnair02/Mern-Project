f877873a57eaa222ec62f6f13bb8285c6a95d2a4
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-27 23:24:32 +0530

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { useState } from "react";
 import axios from "axios";
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import {format} from "timeago.js";
 
 const Container = styled.div`
   width: ${(props) => props.type !== "sm" && "360px"};
@@ -53,34 +52,39 @@ const Info = styled.div`
   font-size: 14px;
   color: ${({ theme }) => theme.textSoft};
 `;
-const Card = (type, video) => {
-  const [channel, setChannel] = useState([]);
+
+const Card = ({ type, video }) => {
+  const [channel, setChannel] = useState({});
+  console.log(video)
   useEffect(() => {
-    const fetchChannels = async () => {
+    const fetchChannel = async () => {
       const res = await axios.get(`/users/find/${video.userId}`);
       setChannel(res.data);
     };
-    fetchChannels();
+    fetchChannel();
   }, [video.userId]);
 
   return (
     <Link to={`/video/${video._id}`} style={{ textDecoration: "none" }}>
       <Container type={type}>
-        <Image type={type} src={video.imgUrl} />
+        <Image
+          type={type}
+          src={video.imgUrl}
+        />
         <Details type={type}>
-          <ChannelImage type={type} src={channel.img} />
+          <ChannelImage
+            type={type}
+            src={channel.img}
+          />
           <Texts>
             <Title>{video.title}</Title>
             <ChannelName>{channel.name}</ChannelName>
-            <Info>
-              {video.views} views • {(video.createdAt)}
-            </Info>
+            <Info>{video.views} views • {format(video.createdAt)}</Info>
           </Texts>
         </Details>
       </Container>
     </Link>
   );
 };
-
 
 export default Card;
