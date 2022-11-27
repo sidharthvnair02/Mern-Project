31588e3296fc233ce5d70fd697be9ca0de74f55e
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-11-27 23:30:48 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+
+import React,{ useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
-import { darkTheme, lightTheme } from "./utils/theme";
+import { darkTheme, lightTheme } from "./utils/Theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
-import SignIn from "./pages/Signin";
-
+import SignIn from "./pages/SignIn";
+import Search from "./pages/Search";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   display: flex;
@@ -19,15 +21,12 @@ const Main = styled.div`
 `;
 const Wrapper = styled.div`
   padding: 22px 96px;
-
 `;
 
-
-
-
-
 function App() {
-   const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
@@ -41,9 +40,13 @@ function App() {
                   <Route index element={<Home type="random" />} />
                   <Route path="trends" element={<Home type="trend" />} />
                   <Route path="subscriptions" element={<Home type="sub" />} />
-                  <Route path="signin" element={<SignIn />} />
+                  <Route path="search" element={<Search />} />
+                  <Route
+                    path="signin"
+                    element={currentUser ? <Home /> : <SignIn />}
+                  />
                   <Route path="video">
-                    <Route path=":id" element={<Video />} />
+                    <Route path=":_id" element={<Video />} />
                   </Route>
                 </Route>
               </Routes>
